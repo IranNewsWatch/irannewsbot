fb9bf22ca2a6175b23626afcc69ee2c245130abe
IranNewsWatch <iranrevolutionwatch@gmail.com>
2022-12-11 21:59:16 -0500

    bug fix

--- app.py
+++ app.py
@@ -4,3 +4,11 @@ newsbot = NewsBot(
     path_to_keys="keys/twitterkeys.yaml", 
     path_to_filters="src/news_filter.yaml",
     )
+
+newsbot = NewsBot(path_to_keys="../keys/twitterkeys.yaml", path_to_filters="../src/news_filter.yaml")
+
+tweet_text = newsbot.collect_news()
+
+print(tweet_text)
+
+newsbot.tweet(mock=True)
