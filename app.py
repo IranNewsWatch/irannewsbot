999d2b240152f1a53f3b1c11d2184f8d1ede334b
IranNewsWatch <iranrevolutionwatch@gmail.com>
2022-12-11 22:02:47 -0500

    path fix

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ newsbot = NewsBot(
     path_to_filters="src/news_filter.yaml",
     )
 
-newsbot = NewsBot(path_to_keys="../keys/twitterkeys.yaml", path_to_filters="../src/news_filter.yaml")
+newsbot = NewsBot(
+    path_to_keys="keys/twitterkeys.yaml",
+    path_to_filters="src/news_filter.yaml",
+    )
 
 tweet_text = newsbot.collect_news()
 
